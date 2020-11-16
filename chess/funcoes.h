4e011be75190510f6a7a02042b40205136d4376e
makucas <46076494+makucas@users.noreply.github.com>
2020-11-16 14:37:45 -0300

    Update funcoes.h

--- chess/funcoes.h
+++ chess/funcoes.h
@@ -10,6 +10,6 @@ void estrutura_borda(struct borda *pb);
 void printa_tabuleiro(char m[8][8], struct borda *pb);
 void move_peca(char m[8][8]);
 int opcoes();
-int converte(int *pn, char *pl);
+int converte(int *pn, char l);
 
 #endif // FUNCOES_H_INCLUDED

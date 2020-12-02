e33a61bd56b3bee2fe46f72f2505aa9bef485134
makucas <46076494+makucas@users.noreply.github.com>
2020-12-02 16:11:48 -0300

    Update main.c

--- chess/main.c
+++ chess/main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
 #include "funcoes.h"
 
-
 int main() {
-    int n, l, c;
+    int x=-1;
+    int vez=0;
     struct borda b;
+    struct jogador j[2];
     char tab[8][8] = {
         { 'T', 'C', 'B', 'Q', 'K', 'B', 'C', 'T', },
         { 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', },
@@ -18,17 +21,59 @@ int main() {
     };
 
 
-
     estrutura_borda(&b);
+    j[0].movimentos = 0;
+    j[0].capturas = 0;
+    j[1].movimentos = 0;
+    j[1].capturas = 0;
+
+    printf("Nome do jogador que utilizara as pecas ");
+    SetColor(12);
+    printf("VERMELHAS   ");
+    SetColor(7);
+    scanf("%s", j[0].name);
+    printf("Nome do jogador que utilizara as pecas ");
+    SetColor(3);
+    printf("AZUIS       ");
+    SetColor(7);
+    scanf("%*c%s", j[1].name);
+    printf("Pecas");
+    SetColor(12);
+    printf(" VERMELHAS ");
+    SetColor(7);
+    printf("movem primeiro!\nPressione qualquer tecla para continuar  \n");
+    getch();
 
     while(1) {
         printa_tabuleiro(tab, &b);
-        n = opcoes();
-        if(n == 2) break;
-        else if(n == 1) {
-            move_peca(tab);
+        if(x == 3) {
+            muda_vez(&vez, x);
+            printf("\nO Rei foi capturado, temos um vencedor\nqualquer tecla para acessar os dados da partida...\n");
+            getch();
+            dados_jogadores(vez, x, j);
+            break;
         }
+        printf("jogam as ");
+        if(vez == 0) {
+            SetColor(12);
+            printf("VERMELHAS ");
+            SetColor(7);
+        }
+        if(vez == 1) {
+            SetColor(3);
+            printf("AZUIS ");
+            SetColor(7);
+        }
+        printf("(%s)\n", j[vez].name);
+        x = move_peca(tab, j, vez);
+        if(x == 0) {
+            printf("movimento invalido, pressione alguma tecla para continuar\n");
+            getch();
+        }
+        muda_vez(&vez, x); //recebe um int que indentifica a vez, e um outro int que indentifica se o movimento anterior foi valido
     }
+
 
     return 0;
 }
+

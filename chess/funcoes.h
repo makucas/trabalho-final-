fcd5cb38a8aa9f70e1e40a782ae3ccfa3618def1
makucas <46076494+makucas@users.noreply.github.com>
2020-12-10 15:09:47 -0300

    Update funcoes.h

--- chess/funcoes.h
+++ chess/funcoes.h
@@ -12,16 +12,31 @@ struct jogador{
     int capturas;
 };
 
-void dados_jogadores(int vez, int x, struct jogador *pj);
+struct REGISTRO{
+    int ni, nf;
+    char ci, cf;
+};
+
+struct LISTA{
+    struct REGISTRO reg;
+    struct LISTA *next;
+};
+
+
+
+void dados_jogadores(int vez, int x, struct jogador *pj, struct LISTA **l);
 int muda_vez(int *vez, int x);
 void SetColor(int ForgC);
+void inicia(struct LISTA **l, struct LISTA **l_b);
+void add(struct LISTA **l);
 void estrutura_borda(struct borda *pb);
 void printa_tabuleiro(char m[8][8], struct borda *pb);
-int move_peca(char m[8][8], struct jogador *pj, int vez);
+int move_peca(char m[8][8], struct jogador *pj, int vez, struct LISTA **l);
 //int opcoes();
 int converte(int *pn, char l);
 int checa_movimento(int l_i, int c_i, int l_f, int c_f, char m[8][8], int vez);
+void lista_movimentos();
+
 
 
 #endif // FUNCOES_H_INCLUDED
-
